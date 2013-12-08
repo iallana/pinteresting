72dabc9aad68281a37af5c5d3c4ef782edd999c5
Irshad Allana <irshad87@gmail.com>
2013-12-07 19:33:18 -0800

    Customize bootstrap variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
   root 'pages#home'
   get 'about' =>'pages#about' #creates about_path
+  get 'products' => 'pages#products' #creates products_path
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
